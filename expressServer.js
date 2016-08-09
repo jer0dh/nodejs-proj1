79f337dfd181a8d4bf397a77ca8d5217a4cf4631
jer0dh <jerod@jhtechservices.com>
2016-08-09 15:21:34 -0500

    changes

--- expressServer.js
+++ expressServer.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
+var list = require('./data/data.json');
 
-app.use('/static', express.static('static'));
+
+app.use(express.static('public'));
+app.use(express.static('node_modules/bootstrap/dist'));
+
 app.get('/', (q,r) => {
-	r.end('Hello from express');
+	r.render('index.jade');
+});
+
+app.get('/list', (q,r) => {
+	r.render('list.jade', {list: list} );
 });
 
 
